8d9c21e351ac554e6f437986b3fc5fc7eb438651
Michael Jonathan <michael.jonathan01@gmail.com>
2020-08-23 17:16:31 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Main extends JFrame {
+
+	 public Main() {
+	  setTitle("Swee Library");
+	  setSize(600, 400);
+	  setLocationRelativeTo(null);
+	  setDefaultCloseOperation(EXIT_ON_CLOSE);
+	  
+	  JLabel lblTitle;
+	  JButton btnLogIn, btnCreateMember;
+	  
+	  lblTitle = new JLabel("Swee Library", JLabel.CENTER);
+	  lblTitle.setFont(new Font("Times New Roman", Font.BOLD, 80));
+	  lblTitle.setPreferredSize(new Dimension(200, 150));
+	  
+	  btnLogIn = new JButton("Log In");
+	  btnLogIn.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+	  btnCreateMember = new JButton("Create Membership");
+	  btnCreateMember.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+	  
+	  JPanel buttonPanel = new JPanel(new GridLayout(2, 1, 15, 15));
+	  buttonPanel.add(btnLogIn);
+	  buttonPanel.add(btnCreateMember);
+	  buttonPanel.setBorder(new EmptyBorder(50, 150, 50, 150));
+
+	  JPanel mainPanel = new JPanel(new BorderLayout(5, 5));
+	  mainPanel.add(lblTitle, BorderLayout.NORTH);
+	  mainPanel.add(buttonPanel, BorderLayout.CENTER);
+	  
+	  add(mainPanel);
+	  
+	  setVisible(true);
+	 }
+	 
+	 public static void main(String[] args) {
+	  // TODO Auto-generated method stub
+	  new Main();
+	 }
+
+	}
