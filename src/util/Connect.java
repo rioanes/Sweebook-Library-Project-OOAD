191c453562cf111a9d9f7df79687e0a2dafb9455
Michael Jonathan <michael.jonathan01@gmail.com>
2020-08-25 20:44:57 +0700

    connect

--- src/util/Connect.java
+++ src/util/Connect.java
@@ -1,5 +1,18 @@
 package util;
 
-public class Connect {
+import java.sql.*;
 
+
+public class Connect {
+	public static Connection connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = 
+					DriverManager.getConnection("jdbc:mysql://localhost/mysql?user=&password=");
+			return conn;
+		}catch(Exception e) {
+			
+		}
+		 return null;
+	}
 }
